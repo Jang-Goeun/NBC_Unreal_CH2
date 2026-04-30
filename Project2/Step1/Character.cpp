aa11201132ede123468256c8828807da57d03395
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-04-30 20:57:06 +0900

    [FEAT] 필수 구현 기능 STEP 2 - 입력 유효성 검사

--- Project2/Step1/Character.cpp
+++ Project2/Step1/Character.cpp
@@ -15,6 +15,37 @@ void printStatus(string name, int stat[]) {
 	cout << "====================================" << endl;
 }
 
+// 상태 정보 입력 메서드
+// - 입력 유효성 검사 진행
+// - 모두 50보다 클 때만 입력 루프 탈출
+// - string inputStat1: 첫번째 입력 스탯 요소 이름
+// - string inputStat2: 두번째 입력 스탯 요소 이름
+void inputStat(int* stat, string inputStat1, string inputStat2) {
+	int stat1, stat2; // 입력받을 스탯 값 저장
+
+	// 스택 입력 반복문
+	while (true) {
+		cout << "Enter " << inputStat1 << " and " << inputStat2 << ": ";
+		cin >> stat1 >> stat2;
+
+		if (stat1 > 50 && stat2 > 50)
+			break;
+		else
+			cout << inputStat1 << " or " << inputStat2 << " is too low. Try again." << endl;
+	}
+
+	// HP와 MP 저장
+	if (inputStat1 == "HP") {  
+		stat[0] = stat1;
+		stat[1] = stat2;
+	}
+	// Attack과 Defense 저장
+	else {
+		stat[2] = stat1;
+		stat[3] = stat2;
+	}
+}
+
 int main() {
 	string name;				// 캐릭터 이름
 	int stat[SIZE] = { 0 };		// 캐릭터 스탯 { "HP", "MP", "공격력", "방어력" }
@@ -30,10 +61,8 @@ int main() {
 	cout << endl;
 
 	// 캐릭터 스택 정보 입력
-	cout << "Enter HP and MP: ";
-	cin >> stat[0] >> stat[1];
-	cout << "Enter Attack and Defense: ";
-	cin >> stat[2] >> stat[3];
+	inputStat(stat, "HP", "MP");
+	inputStat(stat, "Attack", "Defense");
 	cout << endl;
 
 	// 캐릭터 정보 출력 메서드 호출
