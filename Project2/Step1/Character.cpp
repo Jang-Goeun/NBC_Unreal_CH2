5604adb2d27deeca7540b561881cb5498589d668
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-05-01 15:52:25 +0900

    [FEAT] 필수 구현 기능 STEP 3 - 스탯 관리 메뉴

--- Project2/Step1/Character.cpp
+++ Project2/Step1/Character.cpp
@@ -47,26 +47,86 @@ void inputStat(int* stat, string inputStat1, string inputStat2) {
 }
 
 int main() {
+	// 변수 선언
 	string name;				// 캐릭터 이름
 	int stat[SIZE] = { 0 };		// 캐릭터 스탯 { "HP", "MP", "공격력", "방어력" }
+	int HPPotion = 5;			// HP 포션 5개
+	int MPPotion = 5;			// MP 포션 5개
+	bool isGameStart = false;	// 게임 시작/종료 플래그
 
-	// 게임 시작
+
+	// 게임 시작 
 	cout << "===========================================" << endl;
 	cout << "   [ Dungeon Escape Text RPG ]" << endl;
 	cout << "===========================================" << endl;
 
-	// 캐릭터 이름 입력
+	// 캐릭터 정보 입력
 	cout << "Enter your hero's name: ";
 	cin >> name;
 	cout << endl;
 
-	// 캐릭터 스택 정보 입력
 	inputStat(stat, "HP", "MP");
 	inputStat(stat, "Attack", "Defense");
 	cout << endl;
 
-	// 캐릭터 정보 출력 메서드 호출
+	// 캐릭터 정보 출력
 	printStatus(name, stat);
+
+	// 스탯 관리 메뉴
+	cout << "\n* You received " << HPPotion << " HP Potions and " << MPPotion << " MP Potions." << endl;
+	cout << "============================================" << endl;
+	cout << "< Character Upgrade >" << endl;
+	cout << "1. HP UP    2. MP UP    3. Attack x2" << endl;
+	cout << "4. Defense x2  5. Show Stats  0. Start Game" << endl;
+	cout << "============================================" << endl;
+
+	while (!isGameStart) {
+		int choice = -1; // 사용자 선택사항 저장 변수
+		cout << "Choose: ";
+		cin >> choice;
+
+		switch (choice) {
+			case 0:	
+				cout << "Starting the game!\n" << endl;
+				isGameStart = true;
+				break;
+			case 1:
+				if (HPPotion == 0)
+					cout << "There is no HP potion." << endl;
+				else {
+					cout << "* HP increased by 20. (HP Potion used: " << --HPPotion << " left)." << endl;
+					stat[0] += 20;
+				}
+				break;
+
+			case 2:
+				if (MPPotion == 0)
+					cout << "There is no MP potion." << endl;
+				else {
+					cout << "* MP increased by 20. (MP Potion used: " << --MPPotion << " left)." << endl;
+					stat[1] += 20;
+				}
+				break;
+
+			case 3:
+				cout << "* Attack power doubled." << endl;
+				stat[2] *= 2;
+				break;
+
+			case 4:
+				cout << "* Defense power doubled." << endl;
+				stat[3] *= 2;
+				break;
+
+			case 5:
+				printStatus(name, stat);
+				break; 
+
+			default:
+				cout << "You entered an incorrect choice." << endl;
+				break;
+			}
+		}
 
 	return 0;
 }
